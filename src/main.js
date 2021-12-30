56e0d5c7a262508e2da72e40ae6afaac5cc86c31
jorshDev <horladjosh@gmail.com>
2021-12-30 07:46:16 +0100

    Fix: EmailJs 412 error from dashboard.emailjs

--- src/main.js
+++ src/main.js
@@ -12,18 +12,18 @@
 // })
 
 
-function sendMail(e) {
-    e.preventDefault()
+function sendMail() {
+    alert("Claim request sent!");
     var tempParams = {
         from_name: document.getElementById("name").value,
         // to_name: document.getElementById("email").value,
         message: document.getElementById("msg").value,
     };
-    emailjs.send('service_00ui67j', 'template_mvmqwfi', tempParams, 'user_UYKI72Q35QSYVNWdrhB7R').then(function(res) {
-        console.log("success", res.status, res.text)
-        alert("Message has been sent")
-    }, function(err) {
-        console.log(err)
+    emailjs.send('service_soozdyb', 'template_9hvqhzr', tempParams, 'user_UYKI72Q35QSYVNWdrhB7R').then(function(res) {
+        console.log("success", res.status, res.text);
+        alert("Your claim request has been received!");
+    }, (err) => {
+        console.log(err);
     })
     console.log("success", res.status)
     alert('Heeyyyyyyyyyyy')
